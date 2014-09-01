e4b2d9e0c7938d8b2324e008f7c943623b669c70
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2014-09-01 11:52:16 +0100

    Added MixPanel tracking code. Currently tracking landing page button clicks.

---
+++ public/js/landing.js
@@ -0,0 +1,1 @@
+$(function(){$(".wgst-button__create-new-collection").on("click",function(){mixpanel.track("Create new collection")}),$(".wgst-button__open-sample-collection").on("click",function(){mixpanel.track("Open sample collection")})});
