7209ba6162cab654cdd9796f91d9845497da5a1f
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2014-09-09 18:53:54 +0100

    Minified js files.

--- public/js/lib/landing.js
+++ public/js/lib/landing.js
@@ -1,16 +1,1 @@
-$(function() {
-
-	if (! window.chrome) {
-		//$('.wgst-landing-buttons').hide();
-		$('.wgst-chrome-only').removeClass('hide-this');
-	}
-
-	$('.wgst-button__create-new-collection').on('click', function(){
-		mixpanel.track("Create new collection");
-	});
-
-	$('.wgst-button__open-sample-collection').on('click', function(){
-		mixpanel.track("Open sample collection");
-	});
-
-});
+$(function(){window.chrome||$(".wgst-chrome-only").removeClass("hide-this"),$(".wgst-button__create-new-collection").on("click",function(){mixpanel.track("Create new collection")}),$(".wgst-button__open-sample-collection").on("click",function(){mixpanel.track("Open sample collection")})});
--- public/js/lib/utils.js
+++ public/js/lib/utils.js
@@ -1,6 +1,1 @@
-//
-// http://stackoverflow.com/a/7592235
-//
-String.prototype.capitalize = function() {
-	return this.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
-};
+String.prototype.capitalize=function(){return this.replace(/(?:^|\s)\S/g,function(t){return t.toUpperCase()})};
