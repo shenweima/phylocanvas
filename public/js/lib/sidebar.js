b62af4ff0ca05509a69f424788f68df50399759b
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2014-09-24 02:06:07 +0100

    Minified js.

--- public/js/lib/sidebar.js
+++ public/js/lib/sidebar.js
@@ -1,17 +1,1 @@
-$(function(){
-
-	(function(){
-
-		window.WGST.exports.showSidebar = function() {
-			//$('.wgst-sidebar').removeClass('wgst--hide-this');
-			$('[data-wgst-sidebar]').removeClass('wgst--hide-this');
-		};
-
-		window.WGST.exports.hideSidebar = function() {
-			//$('.wgst-sidebar').addClass('wgst--hide-this');
-			$('[data-wgst-sidebar]').addClass('wgst--hide-this');
-		};
-
-	})();
-
-});
+$(function(){!function(){window.WGST.exports.showSidebar=function(){$("[data-wgst-sidebar]").removeClass("wgst--hide-this")},window.WGST.exports.hideSidebar=function(){$("[data-wgst-sidebar]").addClass("wgst--hide-this")}}()});
