2cb7cbd26d94b992f51080fccd4cd07c7d9a53dd
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2014-10-30 22:34:26 +0000

    Added InfoWindow for markers. Refactored code.

--- private/js/lib/map.js
+++ private/js/lib/map.js
@@ -2,60 +2,121 @@ $(function(){
 
 	(function(){
 
+		var openedInfoWindow;
+
 	    var groupAssembliesByPosition = function(collectionId, assemblyIds) {
-	        var groupedPositions = {},
-	            assemblies = WGST.collection[collectionId].assemblies,
+	        var assemblyIdsGroupedByPosition = {},
+	            assemblies = window.WGST.collection[collectionId].assemblies,
 	            assemblyId,
 	            assemblyPositionLatitude,
 	            assemblyPositionLongitude,
 	            assemblyLatLng;
 
-	        for (var i = 0; i < assemblyIds.length; i++) {
-	            assemblyId = assemblyIds[i];
+	        var counter = 0;
+
+	        for (; counter < assemblyIds.length; counter++) {
+	            assemblyId = assemblyIds[counter];
 	            assemblyPositionLatitude = assemblies[assemblyId].ASSEMBLY_METADATA.geography.position.latitude;
 	            assemblyPositionLongitude = assemblies[assemblyId].ASSEMBLY_METADATA.geography.position.longitude;
 	            assemblyLatLng = new google.maps.LatLng(assemblyPositionLatitude, assemblyPositionLongitude);
 
-	            if (typeof groupedPositions[assemblyLatLng.toString()] === 'undefined') {
-	                groupedPositions[assemblyLatLng.toString()] = [];
+	            if (typeof assemblyIdsGroupedByPosition[assemblyLatLng.toString()] === 'undefined') {
+	                assemblyIdsGroupedByPosition[assemblyLatLng.toString()] = [];
 	            }
 	            
-	            groupedPositions[assemblyLatLng.toString()].push(assemblyId);
+	            assemblyIdsGroupedByPosition[assemblyLatLng.toString()].push(assemblyId);
 	        } // for
 
-	        console.log('[WGST] Grouped assemblies by position:');
-	        console.dir(groupedPositions);
+	        console.log('[WGST] Assembly ids grouped by position:');
+	        console.dir(assemblyIdsGroupedByPosition);
 
-	        return groupedPositions;
+	        return assemblyIdsGroupedByPosition;
 	    };
 
-	    var createGroupMarker = function(groupAssemblyIds, groupMarkerLat, groupMarkerLng, groupSize) {
+	    var createGroupInfoWindow = function(groupAssemblies) {
+
+            var templateContext = {
+	            	groupAssemblies: groupAssemblies,
+	            	location: groupAssemblies[0].ASSEMBLY_METADATA.geography.address
+	            },
+            	infoWindowTemplateSource = $('.wgst-template[data-template-id="info-window"]').html(),
+                infoWindowTemplate = Handlebars.compile(infoWindowTemplateSource),
+                infoWindowHtml = infoWindowTemplate(templateContext);
+
+			var infoWindow = new google.maps.InfoWindow({
+				content: '<div class="test">' + infoWindowHtml + '</div>'
+			});
+
+			return infoWindow;
+	    };
+
+	    //var createGroupMarker = function(groupAssemblyIds, groupMarkerLat, groupMarkerLng, groupSize) {
+	    var createGroupMarker = function(groupAssemblies) {
+
+	    	var groupAssemblyIds = groupAssemblies.map(function(assembly){
+	    		return assembly.ASSEMBLY_METADATA.assemblyId;
+	    	});
+
+	    	var groupLatitude = groupAssemblies[0].ASSEMBLY_METADATA.geography.position.latitude,
+		    	groupLongitude = groupAssemblies[0].ASSEMBLY_METADATA.geography.position.longitude;
+
+		    var groupSize = groupAssemblies.length;
+
 	        var markerIcon = '//chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + groupSize + '|00FFFF|000000',
-	            groupPosition = new google.maps.LatLng(groupMarkerLat, groupMarkerLng),
+	            groupPosition = new google.maps.LatLng(groupLatitude, groupLongitude),
 	            groupPositionString = groupPosition.toString();
 
-	        WGST.geo.map.markers.group[groupPositionString] = {
+	        window.WGST.geo.map.markers.group[groupPositionString] = {
 	            assemblyIds: groupAssemblyIds,
 	            marker: {}
 	        };
 
-	        WGST.geo.map.markers.group[groupPositionString].marker = new google.maps.Marker({
-	            position: new google.maps.LatLng(groupMarkerLat, groupMarkerLng),
-	            map: WGST.geo.map.canvas,
+	        window.WGST.geo.map.markers.group[groupPositionString].marker = new google.maps.Marker({
+	            position: groupPosition,
+	            map: window.WGST.geo.map.canvas,
 	            icon: markerIcon,
 	            //draggable: true,
 	            optimized: true // http://www.gutensite.com/Google-Maps-Custom-Markers-Cut-Off-By-Canvas-Tiles
 	        });
 
-	        // Handle marker click
-	        google.maps.event.addListener(WGST.geo.map.markers.group[groupPositionString].marker, 'click', function(e) {
-	            // If there is only one assembly id in a group then open that assembly
-	            if (WGST.geo.map.markers.group[groupPositionString].assemblyIds.length === 1) {
-	                openAssemblyPanel(WGST.geo.map.markers.group[groupPositionString].assemblyIds[0]);
-	            } else {
-	                // Do nothing yet
-	            }
-	        });
+	        //
+	        // Create Info Window for a group of assermbly ids
+	        //
+	        var groupInfoWindow = createGroupInfoWindow(groupAssemblies);
+
+	        //
+	        // Handle marker click - open info window
+	        //
+			google.maps.event.addListener(window.WGST.geo.map.markers.group[groupPositionString].marker, 'click', function() {
+				//
+				// Close previously opened info window
+				//
+				if (typeof openedInfoWindow !== 'undefined') {
+					openedInfoWindow.close();
+				}
+
+				//
+				// Open current info window
+				//
+				groupInfoWindow.open(window.WGST.geo.map.canvas, window.WGST.geo.map.markers.group[groupPositionString].marker);
+			
+				//
+				// Set current info window as opened
+				// 
+				openedInfoWindow = groupInfoWindow;
+			});
+
+	        // //
+	        // // Handle marker click
+	        // //
+	        // google.maps.event.addListener(WGST.geo.map.markers.group[groupPositionString].marker, 'click', function(e) {
+	        //     // If there is only one assembly id in a group then open that assembly
+	        //     if (WGST.geo.map.markers.group[groupPositionString].assemblyIds.length === 1) {
+	        //         openAssemblyPanel(WGST.geo.map.markers.group[groupPositionString].assemblyIds[0]);
+	        //     } else {
+	        //         // Do nothing yet
+	        //     }
+	        // });
 	    };
 
 	    var removeAllGroupMarkers = function() {
@@ -77,40 +138,77 @@ $(function(){
 
 	        var assemblyMarkerBounds = new google.maps.LatLngBounds();
 
+	        //
 	        // Remove existing markers
+	        //
 	        removeAllGroupMarkers();
 
-	        // Create new markers if there are any
+	        //
+	        // Create new markers if there are assemblies selected
+	        //
 	        if (selectedAssemblyIds.length > 0) {
-	            var groupedPositions = groupAssembliesByPosition(collectionId, selectedAssemblyIds),
-	                positionString,
+
+	        	//
+	        	// Group assemblies by position
+	        	//
+	            var assemblyIdsGroupedByPosition = groupAssembliesByPosition(collectionId, selectedAssemblyIds);
+
+	            var positionString,
 	                positionGroup,
-	                assemblies = WGST.collection[collectionId].assemblies,
+	                assemblies = window.WGST.collection[collectionId].assemblies,
 	                assemblyId,
 	                positionGroupLat,
 	                positionGroupLng;
 
-	            for (positionString in groupedPositions) {
-	                positionGroup = groupedPositions[positionString];
-	                assemblyId = positionGroup[0];
+	            //
+	            // Create marker for each position
+	            //
+	            for (positionString in assemblyIdsGroupedByPosition) {
+	            	if (assemblyIdsGroupedByPosition.hasOwnProperty(positionString)) {
 
-	                positionGroupLat = assemblies[assemblyId].ASSEMBLY_METADATA.geography.position.latitude;
-	                positionGroupLng = assemblies[assemblyId].ASSEMBLY_METADATA.geography.position.longitude;
+		        		//
+		        		// Get assembly ids grouped by position
+		        		//
+		                var assemblyIds = assemblyIdsGroupedByPosition[positionString];
 
-	                createGroupMarker(positionGroup, positionGroupLat, positionGroupLng, positionGroup.length);
+		                //
+		                // Get assemblies
+		                //
+		                var assembly;
+	            		var groupAssemblies = [];
 
-	                assemblyMarkerBounds.extend(new google.maps.LatLng(positionGroupLat, positionGroupLng));
+		                assemblyIds.map(function(assemblyId) {
+		                	assembly = assemblies[assemblyId];
+		                	groupAssemblies.push(assembly);
+		                });
+
+		                //
+		                // Create group marker
+		                //
+		                createGroupMarker(groupAssemblies);
+
+		                //
+		                // 
+		                //
+		                positionGroupLat = assembly.ASSEMBLY_METADATA.geography.position.latitude;
+		                positionGroupLng = assembly.ASSEMBLY_METADATA.geography.position.longitude;
+
+		                //
+		                // Extend map bounds
+		                //
+		                assemblyMarkerBounds.extend(new google.maps.LatLng(positionGroupLat, positionGroupLng));
+	            	}
 	            } // for
 	        } // if
 
 	        if (assemblyMarkerBounds.isEmpty()) {
-	            WGST.geo.map.canvas.setCenter(new google.maps.LatLng(48.6908333333, 9.14055555556));
-	            WGST.geo.map.canvas.setZoom(5);
+	            window.WGST.geo.map.canvas.setCenter(new google.maps.LatLng(48.6908333333, 9.14055555556));
+	            window.WGST.geo.map.canvas.setZoom(5);
 	        } else {
 	            // Pan to marker bounds
-	            WGST.geo.map.canvas.panToBounds(assemblyMarkerBounds);
+	            window.WGST.geo.map.canvas.panToBounds(assemblyMarkerBounds);
 	            // Set the map to fit marker bounds
-	            WGST.geo.map.canvas.fitBounds(assemblyMarkerBounds);
+	            window.WGST.geo.map.canvas.fitBounds(assemblyMarkerBounds);
 	        }
 	    };
 
