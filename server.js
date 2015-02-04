47ae492321fddbf7ace61e86675ffa22efd2128f
Richard Goater <richard.goater27@gmail.com>
2015-02-04 15:48:56 +0000

    Correct Path to Views
    Views now reside in the /server folder, express config updated.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var app = express();
 
 app.set('port', process.env.PORT || appConfig.server.node.port);
 app.engine('html', swig.renderFile);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'server', 'views'));
 app.set('view engine', 'html');
 // http://stackoverflow.com/a/19965089
 app.use(bodyParser.json({limit: '500mb'}));
