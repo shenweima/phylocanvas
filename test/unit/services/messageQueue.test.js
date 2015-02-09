3aefb6f057870c8175a8001f5557fc3e0b484813
Richard Goater <richard.goater27@gmail.com>
2015-02-09 11:41:59 +0000

    Test Message Parsing Decorator Function
    Demonstrates the working message parser. This function feels like a
    separate module, hence why the tests are slightly convoluted - will
    probably extract at a later date. Have also moved the mocking bits to
    their own functions to make the tests easier to read.

--- test/unit/services/messageQueue.test.js
+++ test/unit/services/messageQueue.test.js
@@ -2,20 +2,33 @@ var assert = require('assert');
 var sinon = require('sinon');
 var rewire = require('rewire');
 
+var EventEmitter = require('events').EventEmitter;
+
 describe('Service: Message Queue', function () {
+
+  function mockConnectionQueue(messageQueueService, mockQueue) {
+    var rabbit = messageQueueService.__get__('rabbit');
+    rabbit.connection = {
+      queue: sinon.stub().yields(mockQueue)
+    };
+    messageQueueService.__set__('rabbit', rabbit);
+  }
+
+  function createEventEmittingQueue(eventName) {
+    var queue = new EventEmitter();
+    queue.subscribe = queue.on.bind(queue, eventName || 'message');
+    queue.bind = function () {};
+    return queue;
+  }
 
   describe('Feature: Assembly Notification Queue', function () {
 
     it('should bind to the correct messages', function (done) {
       var messageQueueService = rewire('services/messageQueue');
+      var rabbit = require('utils/messageQueueConnection').connect();
 
-      // mocking
       var queueSpy = { bind: sinon.spy() };
-      var rabbit = messageQueueService.__get__('rabbit');
-      rabbit.connection = {
-        queue: sinon.stub().yields(queueSpy)
-      };
-      messageQueueService.__set__('rabbit', rabbit);
+      mockConnectionQueue(messageQueueService, queueSpy);
 
       messageQueueService.newAssemblyNotificationQueue(
         { assemblyId: 'assembly1', collectionId: 'collection1' },
@@ -38,7 +51,43 @@ describe('Service: Message Queue', function () {
           done();
         }
       );
+    });
 
+    it('should parse messages as JSON', function (done) {
+      var messageQueueService = rewire('services/messageQueue');
+
+      var queue = createEventEmittingQueue();
+      mockConnectionQueue(messageQueueService, queue);
+
+      messageQueueService.newAssemblyNotificationQueue(
+        {},
+        function (queue) {
+          queue.subscribe(function (error, message) {
+            assert(error === null);
+            assert(message.hello === 'world');
+            done();
+          });
+          queue.emit('message', { data: JSON.stringify({ hello: 'world' }) });
+        }
+      );
+    });
+
+    it('should surface JSON parsing errors', function (done) {
+      var messageQueueService = rewire('services/messageQueue');
+
+      var queue = createEventEmittingQueue();
+      mockConnectionQueue(messageQueueService, queue);
+
+      messageQueueService.newAssemblyNotificationQueue(
+        {},
+        function (queue) {
+          queue.subscribe(function (error) {
+            assert(error && error.message === 'JSON could not be parsed.');
+            done();
+          });
+          queue.emit('message', { data: '{ hello: world }' });
+        }
+      );
     });
 
   });
