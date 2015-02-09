1b77db2accb6487f0221c4d72b31522eb2810265
Richard Goater <richard.goater27@gmail.com>
2015-02-09 18:19:07 +0000

    Introduce Assembly Model
    The old controller has become the "model", and a new controller has been
    added to handle the HTTP requests and delegate to the model. This allows
    the model functions to be run in a context other than web requests,
    e.g. from other models and controllers, or in unit tests.
    
    The result of this refactor also isolates antibiotic-related
    functions into their own model and routes.
    
    N.B. PR to be created after testing on the VPN.

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
-module.exports = function(app) {
-  app.use(require('./routes/landing.js'));
-  app.use(require('./routes/collection.js'));
-  app.use(require('./routes/assembly.js'));
-  app.use(require('./routes/download.js'));
-  app.use(require('./routes/error.js'));
+module.exports = function (app) {
+  app.use(require('routes/landing'));
+  app.use(require('routes/collection'));
+  app.use(require('routes/assembly'));
+  app.use(require('routes/antibiotic'));
+  app.use(require('routes/download'));
+  app.use(require('routes/error'));
   // TODO:
   //app.use(require('./routes/user.js'));
 };
--- server/routes/assembly.js
+++ server/routes/assembly.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/assembly.js');
+var controller = require('controllers/assembly.js');
 
-//
-// Assembly routes
-//
 router.get('/assembly/:id', controller.get);
-//app.post('/assembly', assembly.getData);
-router.post('/api/assembly', controller.apiGetAssembly);
-router.post('/api/assemblies', controller.apiGetAssemblies);
-router.post('/api/assembly/table-data', controller.apiGetAssemblyTableData);
-//app.post('/api/assemblies', assembly.apiGetAssemblies);
+router.post('/api/assembly', controller.getComplete);
+router.post('/api/assemblies', controller.getMany);
+router.post('/api/assembly/table-data', controller.getTableData);
 router.post('/assembly/add', controller.add);
-router.post('/api/assembly/resistance-profile', controller.apiGetResistanceProfile);
-router.get('/api/all-antibiotics', controller.apiGetAllAntibiotics);
-router.get('/api/assembly/:id/core-result', controller.apiGetCoreResult);
+router.post('/api/assembly/resistance-profile', controller.getResistanceProfile);
+router.get('/api/assembly/:id/core-result', controller.getCoreResult);
 
 module.exports = router;
